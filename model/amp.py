d1dae9c8a51e9544e4016599cd9a9cfc1816ae87
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-19 01:21:42 +0900

    update

--- model/amp.py
+++ model/amp.py
@@ -86,7 +86,7 @@ class AMP_net_Deblock(Module):
         self.register_parameter("A", nn.Parameter(torch.from_numpy(A).float(),requires_grad=False))
         self.register_parameter("Q", nn.Parameter(torch.from_numpy(np.transpose(A)).float(), requires_grad=True))
         for n in range(layer_num):
-            self.denoisers.append(Denoiser(scale=2**(layer_num // 3 - n)))
+            self.denoisers.append(Denoiser(scale=2**(2 - n // 3)))
             self.deblocks.append(Deblocker())
             self.register_parameter("step_" + str(n + 1), nn.Parameter(torch.tensor(1.0),requires_grad=False))
             self.steps.append(eval("self.step_" + str(n + 1)))
