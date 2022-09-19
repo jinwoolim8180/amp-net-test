6b29504d1ae4ebb7d6504d5e8be18b25ee456133
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-19 16:56:35 +0900

    update

--- test.py
+++ test.py
@@ -68,8 +68,8 @@ def get_val_result(model,CS_ratio,phaseNum,save_path, is_cuda=True):
 if __name__ == "__main__":
     model_name = "AMP_Net_K"
     CS_ratios = [10]
-    Phases = [9]
-    phase = 9
+    Phases = [12]
+    phase = 12
 	
     save_path = "./results/generated_images"
 
