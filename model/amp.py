4ce0bd40483df797ba0cb7836ac33e29a7ac3aee
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-20 23:28:31 +0900

    update

--- model/amp.py
+++ model/amp.py
@@ -36,17 +36,15 @@ class Denoiser(Module):
         super().__init__()
         self.scale = scale
         self.W_1 = nn.Conv2d(1, 32, 3, padding=1, bias=False)
-        self.res_1 = nn.Sequential(*[ResBlock(32) for _ in range(n_stage)])
-        self.res_2 = nn.Sequential(*[ResBlock(32) for _ in range(n_stage)])
+        self.res = nn.Sequential(*[ResBlock(32) for _ in range(n_stage)])
         self.W_r = ResBlock(32)
         self.W_2 = nn.Conv2d(32, 1, 3, padding=1, bias=False)
 
     def forward(self, inputs, residual=None):
         inputs = torch.unsqueeze(torch.reshape(inputs.t(), [-1, 33, 33]), dim=1)
         h = self.W_1(inputs)
-        output = self.res_2(h)
         h = F.max_pool2d(h, kernel_size=self.scale, stride=self.scale)
-        h = self.res_1(h)
+        h = self.res(h)
         if residual is not None:
             size = (8, 8)
             if self.scale == 1:
@@ -54,7 +52,7 @@ class Denoiser(Module):
             elif self.scale == 2:
                 size = (16, 16)
             h = h + self.W_r(F.interpolate(residual, size=size))
-        output = self.W_2(output + F.interpolate(h, size=(33, 33)))
+        output = self.W_2(F.interpolate(h, size=(33, 33)))
 
         # output=inputs-output
         output = torch.reshape(torch.squeeze(output), [-1, 33*33]).t()
@@ -123,7 +121,7 @@ class AMP_net_Deblock(Module):
                 (step * torch.matmul(self.A.t(), self.A)) - torch.eye(33 * 33).float().cuda(), noise)
 
             X = self.together(X,S,H,L)
-            X = X - deblocker(X)
+            # X = X - deblocker(X)
             X = torch.cat(torch.split(X, split_size_or_sections=33, dim=1), dim=0)
             X = torch.cat(torch.split(X, split_size_or_sections=33, dim=2), dim=0)
             X = torch.reshape(X, [-1, 33 * 33]).t()
