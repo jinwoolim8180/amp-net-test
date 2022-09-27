85d007e1d9ccfe16a43d563969a3a2724d4a9a2f
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-27 21:53:17 +0900

    customized

--- model/rnn_query.py
+++ model/rnn_query.py
@@ -16,12 +16,12 @@ AMP-Net-K
 
 
 class ResBlock(Module):
-    def __init__(self, n_channels, dilation=1):
+    def __init__(self, n_channels):
         super().__init__()
         self.block = nn.Sequential(
-            nn.Conv2d(n_channels, n_channels, 3, padding=dilation, dilation=dilation, bias=False),
+            nn.Conv2d(n_channels, n_channels, 3, padding=2, dilation=2, bias=False),
             nn.ReLU(inplace=True),
-            nn.Conv2d(n_channels, n_channels, 3, padding=dilation, dilation=dilation, bias=False)
+            nn.Conv2d(n_channels, n_channels, 3, padding=1, bias=False)
         )
         self.relu = nn.ReLU(inplace=True)
 
