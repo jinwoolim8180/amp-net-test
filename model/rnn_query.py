fd9c00e872599557e6cdb6dbd6bdf60df91ee6f5
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-09-29 10:09:45 +0900

    customized

--- model/rnn_query.py
+++ model/rnn_query.py
@@ -38,13 +38,9 @@ class Denoiser(Module):
         self.W_1 = nn.Conv2d(1, 32, 3, padding=1, bias=False)
         self.res_1 = ResBlock(32, 32)
 
-        self.query = ResBlock(32, 32)
-        self.key = ResBlock(32, 32)
-        self.value = nn.Sequential(
-            nn.Conv2d(64, 32, 3, padding=1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding=1, bias=False)
-        )
+        self.query = nn.Conv2d(32, 32, 3, padding=1, bias=False)
+        self.key = nn.Conv2d(32, 32, 3, padding=1, bias=False)
+        self.value = nn.Conv2d(64, 32, 3, padding=1, bias=False)
 
         self.res_3 = ResBlock(32, 32)
         self.W_2 = nn.Conv2d(32, 1, 3, padding=1, bias=False)
