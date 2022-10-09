d638a40b936ff9bbc7b14be727b3440495e57307
jinwoolim8180 <jinwoolim8180@snu.ac.kr>
2022-10-09 11:09:30 +0900

    update

--- test.py
+++ test.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
             A = load_sampling_matrix(CS_ratio)
             model = AMP_net_Deblock(phase,A)
             # model.cuda()
-            model.load_state_dict(torch.load(path), map_location=torch.device('cpu'))
+            model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
             print("Start")
             one_psnr, one_ssim = get_val_result(model,CS_ratio,phase,sub_save_path, is_cuda=False)  # test AMP_net
 
